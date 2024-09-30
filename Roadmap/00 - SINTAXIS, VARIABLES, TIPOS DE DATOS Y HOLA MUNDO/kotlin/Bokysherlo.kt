e90ac7671dce71e086d4e636c01ec875cd9637d3
Bokysherlo <juanjesus_moron@hotmail.es>
2024-09-30 16:44:22 +0200

    Create Bokysherlo.kt
    Acabo de empezar a estudiar, disculpad mi nivel y mis errores. Saludos.

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/kotlin/Bokysherlo.kt
@@ -0,0 +1,43 @@
+//https://kotlinlang.org/
+
+//comentario en una linea
+
+/*
+comentar en varias lineas
+*/
+
+//variable y constantes
+
+//variables
+var coches = 5
+//hay 5 coches
+var coche rapido
+
+//constante o variable de solo lectura
+val fruta = 1
+// hay 1 fruta
+val fruta roja
+
+//tipos de datos primitivos
+//Números Enteros
+var Byte = 120
+var Short = 300
+var Int = 12000
+var Long = 14141
+
+//Puntos flotantes
+var Float = 1.25f
+var double =36.6
+
+//Booleano
+  var boleano = Verdadero
+
+//Cadena
+var cadena = cadena de texto
+
+//Imprimir
+sprintln (´´Hola, Kotlin´´)
+
+//Mi primer intento de programación con mi nviel, que es -10, me ha dejado K.O. haber si puedo seguir con los demas retos, se aceptan y se agradecen consejo.
+
+
